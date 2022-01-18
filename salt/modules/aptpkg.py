f97843484c20c60b2657364410fed3aba619b845
Alexander Graul <agraul@suse.com>
2022-01-18 18:40:40 +0100

    Add "migrated" state and GPG key management functions (#290)
    * rpm_lowpkg: add API for GPG keys
    
    * zypperpkg: do not quote the repo name
    
    * pkgrepo: add migrated function
    
    * pkg: unify apt and rpm API for key repo
    
    aptpkg is the virtual package "pkg" for Debian, and contains some API
    for key management.
    
    This patch add a similar API for zypperpkg and yumpkg, also part of the
    same virtual package, based on the counterpart from rpm_lowpkg API.
    
    Convert test to pytests

--- salt/modules/aptpkg.py
+++ salt/modules/aptpkg.py
@@ -2249,7 +2249,7 @@ def _parse_repo_keys_output(cmd_ret):
     return ret
 
 
-def get_repo_keys(aptkey=True, keydir=None):
+def get_repo_keys(aptkey=True, keydir=None, **kwargs):
     """
     .. versionadded:: 2017.7.0
 
@@ -2371,6 +2371,7 @@ def add_repo_key(
     aptkey=True,
     keydir=None,
     keyfile=None,
+    **kwargs
 ):
     """
     .. versionadded:: 2017.7.0
@@ -2424,7 +2425,6 @@ def add_repo_key(
     if not salt.utils.path.which("apt-key"):
         aptkey = False
     cmd = ["apt-key"]
-    kwargs = {}
 
     # If the keyid is provided or determined, check it against the existing
     # repo key ids to determine whether it needs to be imported.
