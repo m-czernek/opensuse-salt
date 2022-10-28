ff988c28607a5ad956fb51aebc4e5329b5525015
Victor Zhestkov <Victor.Zhestkov@suse.com>
2022-10-28 13:20:13 +0300

    Pass the context to pillar ext modules
    * Pass __context__ to ext pillar
    
    * Add test for passing the context to pillar ext module
    
    * Align the test and pillar to prevent failing test

--- salt/master.py
+++ salt/master.py
@@ -1024,6 +1024,7 @@ class MWorker(salt.utils.process.SignalHandlingProcess):
         self.k_mtime = 0
         self.stats = collections.defaultdict(lambda: {"mean": 0, "runs": 0})
         self.stat_clock = time.time()
+        self.context = {}
 
     # We need __setstate__ and __getstate__ to also pickle 'SMaster.secrets'.
     # Otherwise, 'SMaster.secrets' won't be copied over to the spawned process
@@ -1210,7 +1211,7 @@ class MWorker(salt.utils.process.SignalHandlingProcess):
             self.key,
         )
         self.clear_funcs.connect()
-        self.aes_funcs = AESFuncs(self.opts)
+        self.aes_funcs = AESFuncs(self.opts, context=self.context)
         self.__bind()
 
 
@@ -1271,7 +1272,7 @@ class AESFuncs(TransportMethods):
         "_file_envs",
     )
 
-    def __init__(self, opts):
+    def __init__(self, opts, context=None):
         """
         Create a new AESFuncs
 
@@ -1288,6 +1289,7 @@ class AESFuncs(TransportMethods):
         self.fs_ = None
         self.masterapi = None
         self.cache = None
+        self.context = context
         self.event = salt.utils.event.get_master_event(
             self.opts, self.opts["sock_dir"], listen=False
         )
@@ -1636,6 +1638,7 @@ class AESFuncs(TransportMethods):
             pillarenv=load.get("pillarenv"),
             extra_minion_data=load.get("extra_minion_data"),
             clean_cache=load.get("clean_cache"),
+            context=self.context,
         )
         data = pillar.compile_pillar()
         self.fs_.update_opts()
