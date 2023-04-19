2208a50ba6ac01f6d38c8e47b61060144ded49c8
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2023-04-19 10:41:28 +0100

    Fix version detection and avoid building and testing failures
    Mark Salt 3006 as released (#586)
    
    * Mark Salt 3006 as released
    
    Without this, commands like
    
    ```
    salt '*' salt_version.equal 'Sulfur'
    ```
    
    will not work properly and return False although Salt 3006 is used.
    
    Signed-off-by: Dominik Gedon <dominik.gedon@suse.com>
    
    * Fix detection of Salt codename by salt_version module
    
    * Fix mess with version detection bad version definition
    
    * Add some new and fix unit tests
    
    * Fix SaltStackVersion string for new versions format
    
    * Do not crash when passing numbers to 'salt_version.get_release_number'
    
    * Fix salt_version execution module documentation
    
    ---------
    
    Signed-off-by: Dominik Gedon <dominik.gedon@suse.com>
    Co-authored-by: Pablo Suárez Hernández <psuarezhernandez@suse.com>

--- salt/version.py
+++ salt/version.py
@@ -1,8 +1,6 @@
 """
 Set up the version of Salt
 """
-
-import argparse
 import operator
 import os
 import platform
@@ -931,27 +929,5 @@ def versions_report(include_salt_cloud=False, include_extensions=True):
     yield from info
 
 
-def _parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--next-release", help="Return the next release", action="store_true"
-    )
-    parser.add_argument("--parse", help="Parse the passed string as a salt version")
-    # When pip installing we pass in other args to this script.
-    # This allows us to catch those args but not use them
-    parser.add_argument("unknown", nargs=argparse.REMAINDER)
-    return parser.parse_args()
-
-
 if __name__ == "__main__":
-    args = _parser()
-    if args.next_release:
-        print(__saltstack_version__.next_release())
-    elif args.parse:
-        try:
-            print(SaltStackVersion.parse(args.parse))
-        except Exception as exc:  # pylint: disable=broad-except
-            print(f"Failed to parse '{args.parse}' as a salt version: {exc}")
-            sys.exit(1)
-    else:
-        print(__version__)
+    print(__version__)
