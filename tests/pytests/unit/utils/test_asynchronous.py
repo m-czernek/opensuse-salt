925db6edf6a5ce5b3e4318801c7104dce4da7229
Victor Zhestkov <vzhestkov@suse.com>
2024-05-15 11:48:46 +0200

    Do not call the async wrapper calls with the separate thread
    * Do not run method with the distinct thread
    
    * Move test_asynchronous.py to pytests

--- tests/pytests/unit/utils/test_asynchronous.py
+++ tests/pytests/unit/utils/test_asynchronous.py
@@ -11,9 +11,10 @@ future-based sockets, which back every master-initiated job) raised
 
 import asyncio
 
-import salt.ext.tornado.gen
-import salt.utils.asynchronous as asynchronous
+import tornado.gen
+import tornado.ioloop
 
+import salt.utils.asynchronous as asynchronous
 
 class _LoopProbe:
     """
@@ -27,13 +28,45 @@ class _LoopProbe:
     def __init__(self, io_loop=None):
         pass
 
-    @salt.ext.tornado.gen.coroutine
+    @tornado.gen.coroutine
     def check_loop(self):
         # On Python 3.12+ this raises RuntimeError unless an asyncio loop
         # has been installed on the current thread.  Pre-3.12 it returns
         # (and may auto-create) the loop.
         loop = asyncio.get_event_loop()
-        raise salt.ext.tornado.gen.Return(loop is not None)
+        raise tornado.gen.Return(loop is not None)
+
+class HelperA:
+
+    async_methods = [
+        "sleep",
+    ]
+
+    def __init__(self, io_loop=None):
+        pass
+
+    @tornado.gen.coroutine
+    def sleep(self):
+        yield tornado.gen.sleep(0.1)
+        raise tornado.gen.Return(True)
+
+
+class HelperB:
+
+    async_methods = [
+        "sleep",
+    ]
+
+    def __init__(self, a=None, io_loop=None):
+        if a is None:
+            a = asynchronous.SyncWrapper(HelperA)
+        self.a = a
+
+    @tornado.gen.coroutine
+    def sleep(self):
+        yield tornado.gen.sleep(0.1)
+        self.a.sleep()
+        raise tornado.gen.Return(False)
 
 
 def test_sync_wrapper_thread_has_asyncio_loop_65702():
@@ -48,3 +81,58 @@ def test_sync_wrapper_thread_has_asyncio_loop_65702():
         assert sync.check_loop() is True
     finally:
         sync.close()
+
+
+def test_helpers():
+    """
+    Test that the helper classes do what we expect within a regular asynchronous env
+    """
+    io_loop = tornado.ioloop.IOLoop(make_current=False)
+    ret = io_loop.run_sync(lambda: HelperA().sleep())
+    assert ret is True
+
+    ret = io_loop.run_sync(lambda: HelperB().sleep())
+    assert ret is False
+
+
+def test_basic_wrap():
+    """
+    Test that we can wrap an asynchronous caller.
+    """
+    sync = asynchronous.SyncWrapper(HelperA)
+    ret = sync.sleep()
+    assert ret is True
+
+
+def test_basic_wrap_series():
+    """
+    Test that we can wrap an asynchronous caller and call the method in series.
+    """
+    sync = asynchronous.SyncWrapper(HelperA)
+    ret = sync.sleep()
+    assert ret is True
+    ret = sync.sleep()
+    assert ret is True
+
+
+def test_double():
+    """
+    Test when the asynchronous wrapper object itself creates a wrap of another thing
+
+    This works fine since the second wrap is based on the first's IOLoop so we
+    don't have to worry about complex start/stop mechanics
+    """
+    sync = asynchronous.SyncWrapper(HelperB)
+    ret = sync.sleep()
+    assert ret is False
+
+
+def test_double_sameloop():
+    """
+    Test asynchronous wrappers initiated from the same IOLoop, to ensure that
+    we don't wire up both to the same IOLoop (since it causes MANY problems).
+    """
+    a = asynchronous.SyncWrapper(HelperA)
+    sync = asynchronous.SyncWrapper(HelperB, (a,))
+    ret = sync.sleep()
+    assert ret is False
