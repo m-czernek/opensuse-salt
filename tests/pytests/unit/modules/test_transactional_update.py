0d044b182865c672a50d411e93d7a3c86dd1a59d
Marek Czernek <marek.czernek@suse.com>
2024-08-07 10:28:07 +0200

    Make tests compatible with venv bundle
    Co-authored-by: cmcmarrow <charles.mcmarrow.4@gmail.com>

--- tests/pytests/unit/modules/test_transactional_update.py
+++ tests/pytests/unit/modules/test_transactional_update.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 
 import salt.loader.context
@@ -9,6 +10,10 @@ from tests.support.mock import MagicMock, mock_open, patch
 pytestmark = [
     pytest.mark.skip_on_windows(reason="Not supported on Windows"),
 ]
+
+SALT_CALL_BINARY = "salt-call"
+if os.environ.get('VIRTUAL_ENV'):
+    SALT_CALL_BINARY = f"{os.environ.get('VIRTUAL_ENV')}/bin/salt-call"
 
 
 @pytest.fixture
@@ -386,7 +391,7 @@ def test_call_fails_function():
                 "--continue",
                 "--quiet",
                 "run",
-                "salt-call",
+                SALT_CALL_BINARY,
                 "--out",
                 "json",
                 "-l",
@@ -418,7 +423,7 @@ def test_call_success_no_reboot():
                 "--continue",
                 "--quiet",
                 "run",
-                "salt-call",
+                SALT_CALL_BINARY,
                 "--out",
                 "json",
                 "-l",
@@ -461,7 +466,7 @@ def test_call_success_reboot():
                 "--continue",
                 "--quiet",
                 "run",
-                "salt-call",
+                SALT_CALL_BINARY,
                 "--out",
                 "json",
                 "-l",
@@ -495,7 +500,7 @@ def test_call_success_parameters():
                 "--continue",
                 "--quiet",
                 "run",
-                "salt-call",
+                SALT_CALL_BINARY,
                 "--out",
                 "json",
                 "-l",
--- tests/pytests/unit/states/test_pkgrepo.py
+++ tests/pytests/unit/states/test_pkgrepo.py
@@ -1,7 +1,6 @@
 """
     :codeauthor: Tyler Johnson <tjohnson@saltstack.com>
 """
-
 import pytest
 
 import salt.states.pkgrepo as pkgrepo
@@ -519,7 +518,7 @@ def test_migrated_wrong_method():
     with patch.dict(pkgrepo.__grains__, grains), patch.dict(
         pkgrepo.__salt__, salt_mock
     ):
-        assert pkgrepo.migrated("/mnt", method_="magic") == {
+        assert pkgrepo.migrated("/mnt", method="magic") == {
             "name": "/mnt",
             "result": False,
             "changes": {},
